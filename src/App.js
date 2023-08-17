4d248989948b2b39d03881794eb53116e5ec5702
Ochiengsteven <stevenochieng6305@gmail.com>
2023-08-17 15:36:22 +0300

    STYLES THE NAV

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
 import Books from './components/Books';
 import Categories from './components/categories';
+import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="/categories" element={<Categories />} />
-    </Routes>
+    <>
+      <nav>
+        <h1>Bookstore CMS</h1>
+        <ul>
+          <li><Link to="/">Books</Link></li>
+          <li><Link to="/categories">Categories</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </>
   );
 }
 
