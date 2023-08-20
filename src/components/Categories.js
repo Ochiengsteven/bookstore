89f928f82a4e6161e9cf1b739a0ef6f5d36ac1e8
Ochiengsteven <stevenochieng6305@gmail.com>
2023-08-21 00:16:49 +0300

    ADD CATEGORIES REDUCER

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,14 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useSelector } from 'react-redux'; // Import useSelector from react-redux
 
-export default function categories() {
+export default function Categories() {
+  const categories = useSelector((state) => state.categories);
+
   return (
     <div>
-      categories
+      <h2>Categories</h2>
+      <p>{categories}</p>
     </div>
   );
 }
