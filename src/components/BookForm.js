df8fb3ec59bcc0d93cd9231eca667630226d6328
Ochiengsteven <stevenochieng6305@gmail.com>
2023-08-21 00:10:55 +0300

    ADD FORM ERROR HANDLING

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,12 +1,29 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import './css/bookform.css';
 
 function BookForm({ onAdd }) {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [titleError, setTitleError] = useState(false);
+  const [authorError, setAuthorError] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Validate inputs
+    if (!title) {
+      setTitleError(true);
+    } else {
+      setTitleError(false);
+    }
+
+    if (!author) {
+      setAuthorError(true);
+    } else {
+      setAuthorError(false);
+    }
+
     if (title && author) {
       const newBook = { title, author, id: new Date().getTime().toString() };
       onAdd(newBook);
@@ -25,19 +42,20 @@ function BookForm({ onAdd }) {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
+        {titleError && <div className="error">Please input the title</div>}
         <input
           type="text"
           placeholder="Author"
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
         />
+        {authorError && <div className="error">Please input the author</div>}
         <button type="submit">Add Book</button>
       </form>
     </div>
   );
 }
 
-// Add prop validations
 BookForm.propTypes = {
   onAdd: PropTypes.func.isRequired,
 };
