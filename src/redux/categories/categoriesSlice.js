f3b898c213485c19714378ac66ce474b7f7c5547
Ochiengsteven <stevenochieng6305@gmail.com>
2023-08-22 10:57:04 +0300

    REMOVE DISABLED LINTERS

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,5 +1,3 @@
-// src/redux/categories/categoriesSlice.js
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { createSlice } from '@reduxjs/toolkit';
 
 const categoriesSlice = createSlice({
