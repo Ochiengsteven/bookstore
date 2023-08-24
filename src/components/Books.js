516821640900b4d39925b532263c6d467ec866de
Ochiengsteven <stevenochieng6305@gmail.com>
2023-08-24 21:33:39 +0300

    ADD DELETE BOOK FUNCTION

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,27 +1,36 @@
 // src/components/Books.js
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { useDispatch } from 'react-redux'; // Import useDispatch from react-redux
+import { useDispatch, useSelector } from 'react-redux';
 import BookList from './BookList';
 import BookForm from './BookForm';
-import { addBook, removeBook } from '../redux/books/booksSlice'; // Import the addBook and removeBook actions
+import { addBookAsync, deleteBookAsync } from '../redux/books/booksSlice';
 
 const Books = () => {
-  const dispatch = useDispatch(); // Get the dispatch function from Redux
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books.books); // Get the books from the state
 
-  const handleAddBook = (newBook) => {
-    dispatch(addBook(newBook)); // Dispatch the addBook action with the new book data
+  const handleAddBook = async (newBook) => {
+    try {
+      // eslint-disable-next-line max-len
+      await dispatch(addBookAsync(newBook)); // Dispatch the addBookAsync action with the new book data
+    } catch (error) {
+      console.error('Error adding book:', error);
+    }
   };
 
-  const handleDeleteBook = (id) => {
-    dispatch(removeBook(id)); // Dispatch the removeBook action with the book's id
+  const handleDeleteBook = async (id) => {
+    try {
+      await dispatch(deleteBookAsync(id)); // Dispatch the deleteBookAsync action with the book's id
+    } catch (error) {
+      console.error('Error deleting book:', error);
+    }
   };
 
   return (
     <div>
       <h2>Books</h2>
       <BookForm onAdd={handleAddBook} />
-      <BookList onDelete={handleDeleteBook} />
+      <BookList books={books} onDelete={handleDeleteBook} />
     </div>
   );
 };
